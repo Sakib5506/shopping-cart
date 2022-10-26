50e8a5bc37681087d24d67a3cf9f5113b7bdcd4c
Sakib5506 <38976669+Sakib5506@users.noreply.github.com>
2022-10-26 14:09:11 +0600

    Function Updated

--- app.js
+++ app.js
@@ -1,19 +1,71 @@
-var count = 1;
-
-// Plus button handler
-const plusBtn = document.getElementById('plus-btn');
-plusBtn.addEventListener('click',function(){
-    count++;
-    console.log(count);
-    document.getElementById('input-Iphone').value = count;
-
-    const iphonePrice = 1219 * count;
-    console.log(iphonePrice);
-    document.getElementById('iphone-price').innerText = iphonePrice;
-    const subtotal = iphonePrice;
-    const tax = .05 * iphonePrice;
-    const total = tax + subtotal;
-    document.getElementById('sub-total').innerText = subtotal;
-    document.getElementById('tax').innerText = tax.toFixed(2);
-    document.getElementById('total').innerText = total;
+const phonePlusBtn = document.getElementById('plus-btn');
+phonePlusBtn.addEventListener('click',function(){
+    productCount('phone',true);
 })
+
+const phoneMinusBtn = document.getElementById('minus-btn');
+phoneMinusBtn.addEventListener('click',function(){
+    productCount('phone',false);
+})
+
+const casePlusBtn = document.getElementById('case-plus-btn');
+casePlusBtn.addEventListener('click',function(){
+    productCount('case',true);
+})
+
+const caseMinusBtn = document.getElementById('case-minus-btn');
+caseMinusBtn.addEventListener('click',function(){
+    productCount('case',false);
+})
+
+const closeBtnPhone = document.getElementById('removePhone');
+closeBtnPhone.addEventListener('click',function(){
+    document.getElementById('phone-section').style.display = "none";
+})
+const closeBtnCase = document.getElementById('removeCase');
+closeBtnCase.addEventListener('click',function(){
+    document.getElementById('case-section').style.display = "none";
+    document.getElementById('close').style.display = "none";
+    document.getElementById('close-last').style.display = "none";
+})
+
+
+
+function productCount(item, isPlus){
+    const productInput = document.getElementById(item + '-input');
+    const countInput = parseInt(productInput.value);
+    let newCountInput = countInput;
+    if(isPlus == true){
+        newCountInput = countInput + 1;
+    }else if(isPlus == false && countInput > 0){
+        newCountInput = countInput - 1;
+    }
+    productInput.value = newCountInput;
+
+    let productPrice = 0;
+    if(item == 'phone'){
+        productPrice = newCountInput * 1219;
+    }else if(item == 'case'){
+        productPrice = newCountInput * 59;
+    }
+    document.getElementById(item + '-price').innerText = productPrice;
+    calculateTotal();
+}
+
+function calculateTotal(){
+    const phonePrice = productIdentify('phone');
+    const casePrice = productIdentify('case');
+    const subTotal = phonePrice + casePrice;
+    document.getElementById('sub-total').innerText = subTotal;
+    const tax = (subTotal * .05).toFixed(2);
+    const taxNumber = parseFloat(tax);
+    document.getElementById('tax').innerText = taxNumber;
+    const total = subTotal + taxNumber;
+    document.getElementById('total').innerText = total;
+}
+
+function productIdentify(product){
+    const price = document.getElementById(product + '-price').innerText;
+    const priceNumber = parseInt(price);  
+    return priceNumber;
+}
